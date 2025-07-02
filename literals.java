700853afb7d361b0ef87cceaad4431d048de6ec2
Neha <you@example.com>
2025-07-02 19:33:45 +0530

    .

---
+++ literals.java
@@ -0,0 +1,13 @@
+public class literals {
+    public static void main(String[] args) {
+        //lierals
+
+        int num = 2;
+        int num1 = 0b101;
+        num++;
+        System.out.println(num);
+        System.out.println(num1);
+        
+    }
+    
+}
