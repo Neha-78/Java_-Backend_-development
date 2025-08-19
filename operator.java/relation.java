1da375a00911639d7fb4a914587c74527bc1ec70
Neha <you@example.com>
2025-08-19 21:28:33 +0530

    ...

---
+++ operator.java/relation.java
@@ -0,0 +1,11 @@
+
+public class relation {
+    public static void main(String[] args) {
+        int x = 6;
+        int y =5;
+         
+        boolean result = x!= y;
+        System.out.println(result);
+    }
+}
+a
---
+++ operator.java/test.java
@@ -0,0 +1,21 @@
+
+public class test {
+    public static void main(String[] args){
+        int a=2;
+        int b=10;
+        
+        int sum = a+b;
+        int sub = a-b;
+        int mul = a*b;
+        double div = a/b;
+        
+        boolean check = a>b;
+        
+        boolean result = a>b && b>0 ; 
+        
+        System.out.println("result of all operations is:"+sum+","+sub+","+mul+","+div+","+check+"," +result);
+     
+         // write code here
+         
+    }
+}
